5e77644a8221252cb5c756a3852cfa3c4c228e07
Kerby Keith Aquino <hello@kurojifusky.com>
2024-06-08 00:42:56 +0800

    add those juicy type definitions

--- src/lib/utils/SponsorBlock.ts
+++ src/lib/utils/SponsorBlock.ts
@@ -7,31 +7,104 @@ interface AnyObjectString {
 }
 
 export namespace SponsorBlock {
-  export interface SkipSegments {}
-  export interface LockedSegments {}
+  export type Categories =
+    | "sponsor"
+    | "selfpromo"
+    | "intro"
+    | "interaction"
+    | "preview"
+    | "poi_highlight"
+    | "outro"
+    | "filler"
+    | "exclusive_access"
+    | "music_offtopic"
+    | "chapter"
+
+  export type ActionType = "skip" | "mute" | "poi_highlight" | "chapter"
+
+  export interface SearchSegmentsResponse {
+    segmentCount: number
+    page: number
+    segments: {
+      UUID: string
+      timeSubmitted: number
+      startTime: number
+      endTime: number
+      category: Categories
+      actionType: ActionType
+      votes: number
+      views: number
+      locked: number
+      hidden: number
+      shadowHidden: number
+      userID: string
+      description: string
+    }[]
+  }
+  export interface SkipSegmentsResponse {
+    segment: [number, number]
+    UUID: string
+    category: Categories
+    videoDuration: number
+    actionType: ActionType
+    locked: number
+    votes: number
+    description: string | null
+  }
+  export interface LockedSegmentsResponse {
+    categories: Exclude<Categories, "chapter" | "poi_highlight">
+    reason: string | null
+    actionTypes: ActionType[]
+  }
 }
 
-const parseURL = (baseUrl: string, params: AnyObjectString): string => {
+const concatLiteralArrayToString = (items: string[]) => {
+  const concatItems = items.map((i) => `"${i}"`).join(",")
+  return `[${concatItems}]`
+}
+
+const fetchWithQueryParams = <T>(baseUrl: string, params: AnyObjectString) => {
   const url = new URL(baseUrl)
 
   Object.entries(params).forEach(([key, value]) => {
     url.searchParams.append(key, value)
   })
 
-  return url.toString()
+  return fetch(url.toString()) as Promise<T | string>
 }
 
 export const sb = {
-  getSegments: (videoId: string): Promise<SponsorBlock.SkipSegments> => {
-    return fetch(
-      parseURL(`${SB_BASE_URL}/searchSegments`, { videoID: videoId })
+  getSearchSegments: ({
+    videoId,
+    actionTypes = ["skip", "mute"]
+  }: {
+    videoId: string
+    actionTypes: SponsorBlock.ActionType[]
+  }) => {
+    return fetchWithQueryParams<SponsorBlock.SearchSegmentsResponse>(
+      `${SB_BASE_URL}/searchSegments`,
+      {
+        videoID: videoId,
+        actionTypes: concatLiteralArrayToString(actionTypes)
+      }
     )
   },
-  getLockedSegments: (
-    videoId: string
-  ): Promise<SponsorBlock.LockedSegments> => {
-    return fetch(
-      parseURL(`${SB_BASE_URL}/lockCategories`, { videoID: videoId })
+
+  getSkipSegments: ({ videoId }: { videoId: string }) => {
+    return fetchWithQueryParams<SponsorBlock.SkipSegmentsResponse[]>(
+      `${SB_BASE_URL}/skipSegments`,
+      {
+        videoID: videoId
+      }
+    )
+  },
+
+  getLockedSegments: ({ videoId }: { videoId: string }) => {
+    return fetchWithQueryParams<SponsorBlock.LockedSegmentsResponse>(
+      `${SB_BASE_URL}/lockCategories`,
+      {
+        videoID: videoId
+      }
     )
   }
 }
