80da8e7432d328f87c51e92e5fe4a39436efc999
Kerby Keith Aquino <hello@kurojifusky.com>
2024-09-30 23:15:35 +0800

    adding fetch wrappers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export * from "./cn"
 export * from "./formatters"
+
+export * from "./SponsorBlock"
--- src/utils/moveDom.ts
+++ src/utils/moveDom.ts
@@ -1,4 +1,7 @@
-const moveDom = (node: any, inject: any) => {
+const moveDom = <NT extends Element, NI extends Element | Document>(
+  node: NT,
+  inject: NI
+) => {
   const target = document.querySelector(inject)
   target!.appendChild(node)
 }
