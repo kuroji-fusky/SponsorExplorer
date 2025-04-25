79c35b25fcac92aea7e305962d04b0f2904fe029
Kerby Keith Aquino <hello@kurojifusky.com>
2025-04-26 01:57:49 +0800

    update middleware rule to include other yt image domains

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,7 +5,7 @@ export function middleware(request: NextRequest) {
 
   const csp = `
     script-src 'self' 'unsafe-eval' www.youtube.com 'nonce-${nonce}';
-    connect-src 'self' youtube.googleapis.com *.youtube.com sponsor.ajay.app;
+    connect-src 'self' youtube.googleapis.com *.youtube.com sponsor.ajay.app yt3.ggpht.com i.ytimg.com;
     frame-ancestors 'self' www.youtube-nocookie.com;
     upgrade-insecure-requests;
   `
