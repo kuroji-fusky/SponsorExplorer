465540c35a5920a47891c3c6dea6ed784707177b
Kuro <hello@kurojifusky.com>
2025-05-16 00:06:58 +0800

    add other packages for tailwind containers

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,19 +1,20 @@
 import type { Config } from "tailwindcss"
 import typographyPlugin from "@tailwindcss/typography"
+import containerPlugin from "@tailwindcss/container-queries"
 
 export default {
   experimental: {
-    optimizeUniversalDefaults: true
+    optimizeUniversalDefaults: true,
   },
-  darkMode: ["variant", [
-    "html.dark &",
-    "@media (prefers-color-scheme: dark) { & }"
-  ]],
+  darkMode: [
+    "variant",
+    ["html.dark &", "@media (prefers-color-scheme: dark) { & }"],
+  ],
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     // A specific file where SponBlock classes are stored here
-    "./src/utils/mapCategory.ts"
+    "./src/utils/mapCategory.ts",
   ],
   theme: {
     extend: {
@@ -28,10 +29,16 @@ export default {
           endcards: "var(--sb-endcards)",
           preview: "var(--sb-preview)",
           filler: "var(--sb-filler)",
-          "non-music": "var(--sb-non-music)"
-        }
-      }
-    }
+          "non-music": "var(--sb-non-music)",
+        },
+      },
+      containers: {
+        "screen-2xl": "1536px",
+        "screen-xl": "1280px",
+        "screen-lg": "1024px",
+        "screen-md": "768px",
+      },
+    },
   },
-  plugins: [typographyPlugin]
+  plugins: [typographyPlugin, containerPlugin],
 } satisfies Config
