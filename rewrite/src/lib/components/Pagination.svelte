4d3a0e1ce77bd69c76283d02f258da238d8f6427
Kuro <hello@kurojifusky.com>
2025-12-09 21:55:21 +0800

    added basic pagination lmao

---
+++ rewrite/src/lib/components/Pagination.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { ChevronLeftIcon, ChevronRightIcon } from "@lucide/svelte";
+
+  interface Props {
+    current?: number;
+    total?: number;
+
+    basePath: string;
+  }
+
+  const { current: pgCurrent = 1, total: pgTotal = 1 }: Props = $props();
+</script>
+
+<div id="pagination-container" class="inline-flex items-center *:p-2">
+  <a href={"IBOL"} aria-label="Go back to previous page">
+    <ChevronLeftIcon />
+  </a>
+  {#each [...Array(4)] as _}
+    <a href="IBOL" aria-label={`Go to page ${_}`}> 79 </a>
+  {/each}
+  <a href={"IBOL"} aria-label="Go to next page">
+    <ChevronRightIcon />
+  </a>
+</div>
