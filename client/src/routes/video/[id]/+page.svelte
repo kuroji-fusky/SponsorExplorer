4ed0ba9a8b89f9a2120fdc6dc716dbbcc35bb21a
Kuro <hello@kurojifusky.com>
2025-12-18 10:47:21 +0800

    oops, forgot to include

--- client/src/routes/video/[id]/+page.svelte
+++ client/src/routes/video/[id]/+page.svelte
@@ -1,9 +1,17 @@
 <script>
-  import { Meta, YoutubePlayer } from "$lib/components";
+  import { Meta } from "$lib/components";
   import { page } from "$app/state";
+  import { setContext } from "svelte";
+  import VideoInfo from "$lib/components/panels/VideoInfo.svelte";
+  import { SubmissionTable } from "$lib/components/tables";
+  import { SegmentShelf } from "$lib/components/shelf";
+
+  setContext("videoId", { id: page.params.id });
 </script>
 
 <Meta title="Segments for ((video))" />
-<main class="@container w-full mx-auto max-w-7xl px-6">
-  <YoutubePlayer id={page.params.id} />
+<main class="@container w-full mx-auto max-w-screen-2xl px-6 space-y-2">
+  <VideoInfo />
+  <SegmentShelf />
+  <SubmissionTable />
 </main>
